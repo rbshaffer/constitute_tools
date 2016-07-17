83372e07636aad4b132a9cfda128ac7d38d76865
rbshaffer <rbshaffer@utexas.edu>
2016-07-16 22:50:32 -0500

    Package organization.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='constitute_tools',
+    version='2.0',
+    packages=['constitute_tools'],
+    url='https://www.constituteproject.org/',
+    license='MIT',
+    author='Robert Shaffer',
+    author_email='rbshaffer@utexas.edu',
+    description='Auxiliary tools for the Constitute backend.'
+)
